bfa507cae0ae9c1b19f2c16bb9ed902b6e63e147
Gerard Fulton <cgfulton@gmail.com>
2021-01-20 12:49:43 -0800

    Update service-mesh-features-quick-start.sh

--- service-mesh-features-quick-start.sh
+++ service-mesh-features-quick-start.sh
@@ -1,1 +1,197 @@
+#!/bin/bash
 
+########################
+# include the magic
+########################
+. ./demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=100
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+DEMO_PROMPT="${BLACK}➜ ${CYAN}\W "
+
+# text color
+DEMO_CMD_COLOR=$BLACK
+
+
+# hide the evidence
+clear
+
+export CONTROL_PLANE_NAMESPACE=istio-system
+export BOOKINFO_NAMESPACE=bookinfo
+export BOOKINFO_MESH_USER=bookinfo-mesh-user
+export BOOKINFO_APP_YAML=https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/platform/kube/bookinfo.yaml
+export BOOKINFO_DEST_RULES=https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/networking/destination-rule-all.yaml
+export GATEWAY_CONFIG=https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/networking/bookinfo-gateway.yaml
+
+# Deploy ElasticSearch Operator
+oc apply -n openshift-operators -f- <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: elasticsearch-subscription
+  namespace: openshift-operators
+spec:
+  channel: '4.6'
+  installPlanApproval: Automatic
+  name: elasticsearch-operator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+EOF
+
+# Deploy Jaeger Operator
+oc apply -n openshift-operators -f- <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: jaeger-product-subscription
+  namespace: openshift-operators
+spec:
+  channel: stable
+  installPlanApproval: Automatic
+  name: jaeger-product
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: jaeger-operator.v1.20.2
+EOF
+
+# Deploy Kiali Operator
+oc apply -n openshift-operators -f- <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: kiali-ossm
+  namespace: openshift-operators
+spec:
+  channel: stable
+  installPlanApproval: Automatic
+  name: kiali-ossm
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: kiali-operator.v1.24.4
+EOF
+
+# Deploy Service Mesh Operator
+oc apply -n openshift-operators -f- <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: servicemeshoperator
+  namespace: openshift-operators
+spec:
+  channel: stable
+  installPlanApproval: Automatic
+  name: servicemeshoperator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: servicemeshoperator.v2.0.1.1
+EOF
+
+# Create control plane namespace
+oc new-project $CONTROL_PLANE_NAMESPACE
+
+# Deploy Tools
+oc apply -n $CONTROL_PLANE_NAMESPACE -f- <<EOF
+apiVersion: maistra.io/v2
+kind: ServiceMeshControlPlane
+metadata:
+  name: basic
+  namespace: ${CONTROL_PLANE_NAMESPACE}
+spec:
+  version: v2.0
+  tracing:
+    type: Jaeger
+    sampling: 10000
+  addons:
+    jaeger:
+      name: jaeger
+      install:
+        storage:
+          type: Memory
+    kiali:
+      enabled: true
+      name: kiali
+    grafana:
+      enabled: true
+EOF
+
+# Verify the service mesh is running
+oc get smcp -n $CONTROL_PLANE_NAMESPACE
+
+# Create the bookinfo namespace
+oc new-project $BOOKINFO_NAMESPACE
+
+# Create the Service Mesh Member Roll
+oc apply -n ${CONTROL_PLANE_NAMESPACE} -f- <<EOF
+apiVersion: maistra.io/v1
+kind: ServiceMeshMemberRoll
+metadata:
+  name: default
+  namespace: ${CONTROL_PLANE_NAMESPACE}
+spec:
+  members:
+    # a list of projects joined into the service mesh
+    - ${BOOKINFO_NAMESPACE}
+EOF
+
+# Create a service mesh user for the bookinfo namespace
+oc create user $BOOKINFO_MESH_USER
+
+# Create a service mesh roll binding between the servic mesh users and the control plane
+oc apply -n ${CONTROL_PLANE_NAMESPACE} -f- <<EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  namespace: ${CONTROL_PLANE_NAMESPACE}
+  name: mesh-users
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: mesh-user
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: User
+  name: ${BOOKINFO_MESH_USER}
+
+# Deploy the bookinfo appliacation
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l service=reviews            # reviews Service 
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l account=reviews            # reviews ServiceAccount 
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l app=reviews,version=v1     # reviews-v1 Deployment
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l service=ratings            # ratings Service
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l account=ratings            # ratings ServiceAccount
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l app=ratings,version=v1     # ratings-v1 Deployment
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l service=details            # details Service
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l account=details            # details ServiceAccount
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l app=details,version=v1     # details-v1 Deployment
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l service=productpage        # productpage Service
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l account=productpage        # productpage ServiceAccount
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_APP_YAML -l app=productpage,version=v1 # productpage-v1 Deployment
+
+# Deploy the service mesh gateway
+oc apply -n $BOOKINFO_NAMESPACE -f $GATEWAY_CONFIG 
+export GATEWAY_URL=$(oc -n $CONTROL_PLANE_NAMESPACE get route istio-ingressgateway -o jsonpath='{.spec.host}') 
+
+# Deploy the service mesh destination rules
+oc apply -n $BOOKINFO_NAMESPACE -f $BOOKINFO_DEST_RULES
+
+clear
+echo "GATEWAY_URL: $GATEWAY_URL
+oc get pods -n $BOOKINFO_NAMESPACE
+curl -o /dev/null -s -w "%{http_code}\n" http://$GATEWAY_URL/productpage
+oc get route -n $CONTROL_PLANE_NAMESPACE
+
+# show a prompt so as not to reveal our true nature after
+# the quick-start has concluded
+p ""
